b51f11cb7f250e97b817683ecc74b4caf8379603
Lilja Kettunen <lilkettu@dhcp-asv-110.helsinki.fi>
2015-05-25 14:57:51 +0300

    ?

---
+++ Triple-Town/Triple-Town/src/main/java/tripletown/kayttoliittyma/Tekstikayttoliittyma.java
@@ -0,0 +1,103 @@
+package tripletown.kayttoliittyma;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import tripletown.pala.Pala;
+import tripletown.sovellus.Peli;
+
+public class Tekstikayttoliittyma {
+
+    Peli peli;
+    private final Scanner lukija;
+    private final String[] palat = {"", "ruoho", "pensas", "puu", "talo", "kartano", "linna"};
+
+    public Tekstikayttoliittyma() {
+        this.peli = new Peli();
+        this.lukija = new Scanner(System.in);
+    }
+
+    public void kaynnista() {
+        peli.alustaPelilauta();
+
+        while (true) {
+
+            System.out.println("Pisteet: " + peli.pistetilanne());
+            peli.liikutaKarhua();
+            tulostaLauta();
+            
+            if (pelilautaTaynna()) {
+                System.out.println("Peli loppui.");
+                break;
+            }
+
+            while (true) {
+
+                int pala = peli.arvoPala();
+                System.out.println("Mihin ruutuun haluat asettaa palan " + palat[pala] + "? Sarake:");
+                int x = kysyKoordinaatti();
+                System.out.println("Rivi:");
+                int y = kysyKoordinaatti();
+
+                Pala asetettu = this.peli.asetaPala(pala, x, y);
+
+                if (asetettu != null) {
+                    peli.vierekkaistenPalojenHaku(asetettu);
+                    break;
+                }
+
+                System.out.println("Ruutu ei ollut tyhjä!");
+            }
+            
+        }
+    }
+
+    //Varmistaa että pelaajan antama luku on 0:n ja 5:n väliltä
+    public int kysyKoordinaatti() {
+        int luku = -1;
+
+        while (true) {
+
+            try {
+                luku = Integer.parseInt(lukija.nextLine());
+            } catch (Exception e) {
+            }
+
+            if (luku >= 0 && luku <= 5) {
+                return luku;
+            }
+
+            System.out.println("Anna luku väliltä 0-5");
+        }
+    }
+
+    private void tulostaLauta() {
+
+        System.out.println("  0 1 2 3 4 5");
+
+        for (int y = 0; y < 6; y++) {
+            System.out.print(y + "|");
+            for (int x = 0; x < 6; x++) {
+
+                if (peli.getRuutu(x, y) == null) {
+                    System.out.print("_|");
+                } else {
+                    System.out.print(peli.getRuutu(x, y) + "|");
+                }
+            }
+            System.out.println("");
+        }
+    }
+
+    private boolean pelilautaTaynna() {
+
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                if (peli.getRuutu(x, y) == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
---
+++ Triple-Town/Triple-Town/src/test/java/tripletown/pala/KarhuTest.java
@@ -0,0 +1,56 @@
+
+package tripletown.pala;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class KarhuTest {
+    Karhu karhu;
+    public KarhuTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        karhu = new Karhu(1,1);
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void palaPalauttaaOikeanMerkkijonoesityksen() {
+        assertEquals("K", karhu.toString());
+    }
+    
+    @Test
+    public void palaPalauttaaOikeanPistemaaran() {
+        assertEquals(0, karhu.getPisteet());
+    }
+    
+    @Test
+    public void palaPalauttaaOikeatKoordinaatit() {
+        assertEquals(1, karhu.getX());
+        assertEquals(1, karhu.getY());
+    }
+    
+    @Test
+    public void liikuLiikuttaaKarhuaOikein() {
+        karhu.liiku(2, 2);
+        assertEquals(2, karhu.getX());
+        assertEquals(2, karhu.getY());
+    }
+}
